38959460c5d8dd22100bcf053206f2e305d86a0c
Dong <690443961@qq.com>
2020-10-30 17:08:21 +0800

    1、导入fdsst 2、更新SSE2NEON.h文件 3、arm平台，注释掉sse.hpp里的emmintrin.h,再将SSE2NEON.h include进来 4、导入fdsst算法的源码里用到了SSE汇编指令加速，用arm的NEON指令集实现SSE的同名函数 Q:error: "NEON support not enabled" A:arguments "-DANDROID_ARM_NEON=TRUE", "-DANDROID_TOOLCHAIN=clang"

---
+++ app/src/main/cpp/trackingTargetForFDSST.cpp
@@ -0,0 +1,13 @@
+//
+// Created by dongsiyuan on 2020/10/30.
+//
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/tracking/tracker.hpp>
+#include <string>
+#include <fstream>
+#include <time.h>
+#include "fdssttracker.hpp"
+
+
+
